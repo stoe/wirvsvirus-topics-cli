9301a75368c9e13463964e79e6fe553c556ebec5
Stefan Stölzle <stoe@github.com>
2020-03-29 14:36:22 +0200

    🐛 FIX: Add GITHUB_TOKEN env var to test

--- __test__/index.js
+++ __test__/index.js
@@ -2,7 +2,7 @@ const test = require('ava')
 const execa = require('execa')
 
 test('wirvsvirus-topics', async (t) => {
-  const {stdout} = await execa('./index.js', ['-v', '--token test'])
+  const {stdout} = await execa('./index.js', ['-h'])
 
   t.true(stdout.length > 0)
 })
